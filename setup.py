3cfce8e76750057acaba00eebfa031177000cd52
Iacob Casian <casiacob@gmail.com>
2025-03-25 15:53:06 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cfh",
+    version="0.0.1",
+    author="Casian Iacob",
+    author_email="casian.iacob@aalto.fi",
+    description="",
+    install_requires=["jax", "matplotlib"],
+    zip_safe=False,
+)
